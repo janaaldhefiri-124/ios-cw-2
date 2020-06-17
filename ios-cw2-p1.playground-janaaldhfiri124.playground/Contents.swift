597a0331eb35af37fc123916913681fbbbbabe1c
جنى مسيلم الظفيري124 <janamusailem2@gmail.com>
2020-06-17 20:51:50 +0300

    done

---
+++ ios-cw2-p1.playground-janaaldhfiri124.playground/Contents.swift
@@ -0,0 +1,8 @@
+let family = ["salma" , "nada" , "aziz" , "jana"]
+var evens =  [2, 4, 6, 8, 10, 12]
+var grades = [98.75, 99.0, 98.4]
+var trueFalse = [true, true, true, false]
+print( family[0] ,family[3] )
+print( evens[0] ,evens[5] )
+print( grades[0] ,grades[2] )
+print( trueFalse[0] ,trueFalse[3] )
